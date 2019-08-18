8fd06dc5f13991266f9d0ec6d3accc26c4515b78
Battleroid <casey@caseyweed.com>
2019-08-18 00:28:16 -0400

    Consolidate files, allow spawning via egg

--- src/main/java/com/caseyweed/goawayphantom/Goawayphantom.java
+++ src/main/java/com/caseyweed/goawayphantom/Goawayphantom.java
@@ -1,23 +1,38 @@
 package com.caseyweed.goawayphantom;
 
-import com.caseyweed.goawayphantom.events.SpawnEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
-public final class Goawayphantom extends JavaPlugin {
-
-     private Logger log = Bukkit.getLogger();
+public final class Goawayphantom extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        this.log.info("Started");
-        Bukkit.getPluginManager().registerEvents(new SpawnEvent(), this);
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("Started");
+    }
+
+    public boolean isFullMoon(float time) {
+        int days = (int) time / 24000;
+        int phase = days % 8;
+        return phase == 0;
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        // don't bother if it's full moon
+        if (isFullMoon(event.getLocation().getWorld().getFullTime()))
+            return;
+
+        // if spawned naturally, deny spawn, otherwise allow (such as spawn eggs, spawners, etc)
+        if (event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.NATURAL))
+            event.setCancelled(true);
     }
 
     @Override
     public void onDisable() {
-        this.log.info("Stopped");
+        getLogger().info("Stopped");
     }
 }
